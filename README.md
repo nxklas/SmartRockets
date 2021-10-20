700e1a2f6f311461b4bdfc1c3a97b180a1c359a5
nxklas <niklas-bayer@gmx.de>
2021-10-20 13:17:01 +0200

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smart Rockets
+
+This project contains a implementation of a [genetic algorithm](https://en.wikipedia.org/wiki/genetic_algorithm) that teaches rockets how to fly over several generations
---
+++ SmartRockets/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Forms;
+
+namespace SmartRockets
+{
+    /// <summary>
+    /// Provides the main entry point for the application
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
